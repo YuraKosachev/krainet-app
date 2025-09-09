a5ce6fe06daa4fc05447abb755a76b60182658ac
Косачёв Юрий <y.kosachou@sam-solutions.com>
2025-09-09 21:55:29 +0300

    fix docker compose

--- init/init-multiple-dbs.sql
+++ init/init-multiple-dbs.sql
@@ -1,7 +1,2 @@
- IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'krainet_db') THEN
-        CREATE DATABASE krainet_db;
-    END IF;
-
- IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'notify_db') THEN
-        CREATE DATABASE notify_db;
-    END IF;
+CREATE DATABASE krainet_db;
+CREATE DATABASE notify_db;
